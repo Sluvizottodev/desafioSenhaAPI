d308861e0a3783a4ccaff1791443f8a63841676a
SLuvizotto <luvizottodev@gmail.com>
2024-11-22 09:48:00 -0300

    API sem isPasswordStrong

--- lib/alteracoes/controllers/senhaController.dart
+++ lib/alteracoes/controllers/senhaController.dart
@@ -6,7 +6,7 @@ class SenhaController {
 
   Future<SenhaModel?> validatePassword(String password) async {
     print('Iniciando a validação da senha: $password');  // Log para verificar o valor da senha recebida
-    if (_isPasswordStrong(password)) {
+    if (password!= null) {
       print('Senha forte, enviando para a API...');
       final response = await _apiService.validatePassword(password);
       print('Resposta da API: ${response?.message}');  // Log para verificar a resposta da API
@@ -17,12 +17,12 @@ class SenhaController {
     }
   }
   ///A senha '#eforTe1' está dando erro na API
-  bool _isPasswordStrong(String password) {
-    final regex = RegExp(
-      r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
-    );
-    final isValid = regex.hasMatch(password);
-    print('Senha valida: $isValid');  // Log para verificar se a senha corresponde ao padrão
-    return isValid;
-  }
+  // bool _isPasswordStrong(String password) {
+  //   final regex = RegExp(
+  //     r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,}$',
+  //   );
+  //   final isValid = regex.hasMatch(password);
+  //   print('Senha valida: $isValid');  // Log para verificar se a senha corresponde ao padrão
+  //   return isValid;
+  // }
 }
